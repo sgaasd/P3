30969293365d29947cc158dbace348b5473674dd
Simon Gaasdal <sgaasd19@student.aau.dk>
2020-11-16 21:52:15 +0100

    Add dynamixel lib

--- P3.ino
+++ P3.ino
@@ -1,1 +1,25 @@
-#include "src/libs/EMG_Lib/EMG.h"  
+#include "src/libs/EMG_Lib/EMG.h"
+#include "src/libs/IntervalTimer/IntervalTimer.h"
+
+EMGclass xbee;
+
+void xBeeRead(){
+  xbee.updateData();
+}
+ 
+void setup() {
+  Serial.begin(57600);
+  xbee.begin(Serial1, 115200);
+  }
+
+  
+IntervalTimer timer(10, xBeeRead);
+
+void loop() {
+    while(!Serial1){}
+    while(!Serial){}
+
+  timer.update();
+  Serial.println(xbee.getEMG_CH1());
+  delay(25);
+}
